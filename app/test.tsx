82eb8385fbac574463a6ed769f500eaf62278b94
fbhk12 <97927406+fbhk12@users.noreply.github.com>
2025-01-10 16:56:15 -0600

    Update test.tsx

--- app/test.tsx
+++ app/test.tsx
@@ -1,1 +1,17 @@
-
+export default function Home() {
+  return (
+    <div 
+      className="min-h-screen relative"
+      style={{
+        backgroundImage: 'url(/lambeau68.jpg)',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+      }}
+    >
+      <div className="relative z-10 bg-white/80"> {/* semi-transparent white overlay */}
+        {/* Rest of your content */}
+      </div>
+    </div>
+  )
+}
