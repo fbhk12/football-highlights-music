5cc539853591fb40b22fd00070d74ba4dd384fe3
fbhk12 <97927406+fbhk12@users.noreply.github.com>
2025-01-13 15:43:40 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-};
+  images: {
+    unoptimized: true,
+    domains: ['raw.githubusercontent.com']
+  }
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
