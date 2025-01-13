6bfe641450fe1413a9e1698c3145d83d166fbcb3
fbhk12 <97927406+fbhk12@users.noreply.github.com>
2025-01-13 15:49:26 -0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,4 @@
-import { Playfair_Display } from 'next/font/google'
-
-const playfair = Playfair_Display({
-  subsets: ['latin'],
-  variable: '--font-display'
-})
+import './globals.css'
 
 export default function RootLayout({
   children,
@@ -11,7 +6,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className={`${playfair.variable}`}>
+    <html lang="en">
       <body>{children}</body>
     </html>
   )
