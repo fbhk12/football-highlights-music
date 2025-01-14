53dfff7141b5ff8bd284cba477b1d5e4d0a47658
fbhk12 <97927406+fbhk12@users.noreply.github.com>
2025-01-14 16:19:21 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -13,6 +13,7 @@ export default function Home() {
             width: '100%',
             height: '100%',
             objectFit: 'cover',
+            opacity:  '0.0',
           }}
         />
       </div>
