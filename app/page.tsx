7e3754788190ac9f051cff279d42982007477a2f
fbhk12 <97927406+fbhk12@users.noreply.github.com>
2025-02-18 15:16:10 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -103,7 +103,7 @@ export default function Home() {
         textTransform: 'uppercase',
         letterSpacing: '0.1em',
         fontWeight: 700,
-        marginTop: '2rem',  
+        marginTop: '2.5rem',  
         marginBottom:  '3rem', //increase to 4rem moved icon boxes down
         textShadow: `
           -2px -2px 0 #000,  
