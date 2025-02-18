06338efee242cfdaa80944a3c1b2c81a0ea2da18
fbhk12 <97927406+fbhk12@users.noreply.github.com>
2025-02-18 14:42:51 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -103,7 +103,7 @@ export default function Home() {
         textTransform: 'uppercase',
         letterSpacing: '0.1em',
         fontWeight: 700,
-        marginBottom:  '1.5rem',
+        marginBottom:  '2.5rem',
         textShadow: `
           -2px -2px 0 #000,  
            2px -2px 0 #000,
