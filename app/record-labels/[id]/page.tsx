4cc2a04b0287593ecf9cfee5f557c4943228eadb
fbhk12 <97927406+fbhk12@users.noreply.github.com>
2025-02-20 12:25:24 -0600

    Update page.tsx

--- app/record-labels/[id]/page.tsx
+++ app/record-labels/[id]/page.tsx
@@ -42,7 +42,7 @@ export default function RecordLabelPage({ params }: RecordLabelPageProps) {
             backgroundColor: 'rgba(0, 0, 0, 0.5)',
             padding: '1rem'
           }} className="text-2xl font-bold">
-            KPM Albums With FHM Songs
+            Albums With FHM Songs
           </h2>
           
           <div>
