1557e69122f69e5f3b056cc1285994a3c594e418
fbhk12 <97927406+fbhk12@users.noreply.github.com>
2025-02-23 17:09:11 -0600

    Update page.tsx

--- app/record-labels/[id]/page.tsx
+++ app/record-labels/[id]/page.tsx
@@ -96,8 +96,8 @@ export default function RecordLabelPage({ params }: RecordLabelPageProps) {
                   <Image
                     src="/KPM-cover-green-front.jpg"
                     alt="KPM Album"
-                    width={80}
-                    height={80}
+                    width={60}
+                    height={60}
                     style={{ objectFit: 'cover' }}
                   />
                 </div>
